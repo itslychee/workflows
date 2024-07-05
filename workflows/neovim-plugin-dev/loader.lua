94fb3460ccec9795312de298d1da6892f2add62f
itslychee <itslychee@protonmail.com>
2024-07-04 23:57:43 -0500

    workflows: add neovim-plugin-dev workflow

---
+++ workflows/neovim-plugin-dev/loader.lua
@@ -0,0 +1,6 @@
+vim.opt.rtp:prepend(vim.uv.cwd())
+-- Add your necessary plugin requires below
+--
+-- require "plugin".setup { ... }
+--
+-- Test the plugin through `nix run nixpkgs#neovim -- -u loader.lua`
